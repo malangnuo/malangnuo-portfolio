6098a515f86574876249761102c3923d78c641a3
malangnuo <malangnuo@gmail.com>
2026-02-19 17:58:27 +0900

    Feat education.ts

---
+++ src/data/education.ts
@@ -0,0 +1,21 @@
+export const EDUCATION = [
+    {
+        title: "CJ Cloud Wave",
+        description:
+            "BootCamp Certificate",
+        date: "2023.12 ~ 2024.03",
+    },
+    {
+        title: "Incheon National University",
+        description:
+            "BS, Computer Science",
+        date: "2020.03 ~ 2024.08",
+    },
+    {
+        title: "인천하늘고등학교",
+        description:
+            "Haneul Academy Graduation",
+        date: "2016.03 ~ 2019.02",
+    },
+];
+ 

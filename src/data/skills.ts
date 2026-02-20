92feaeceab227a05e85cb89118146da7e2661337
malangnuo <malangnuo@gmail.com>
2026-02-20 11:24:36 +0900

    Feat Skiils & Languages

---
+++ src/data/skills.ts
@@ -0,0 +1,12 @@
+export const SKILLS = [
+    {
+        title: "Cloud Technologies",
+        description: "AWS, NCP, KUBERNETES"
+
+    },
+    {
+        title: "English",
+        description: "TOEIC Speaking AL (170pt)"
+    },
+];
+ 
